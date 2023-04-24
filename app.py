0b7ceb41ed45a398bba9f452998a6a93fb33e0ef
Nginze <kuugjonathan45@gmail.com>
2023-04-24 23:20:01 +0000

    ssl

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ app.register_blueprint(post, url_prefix="/posts")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, debug=True, host='0.0.0.0', port=port)
+    socketio.run(app, debug=True, host='0.0.0.0', port=port, ssl_context='adhoc')
